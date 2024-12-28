7cc6fd2b844980b328e408ef48f9187b91a723ab
hong <pengjianhongpku@126.com>
2024-12-28 12:28:55 +0800

    chapter 7 done

--- app/query/route.ts
+++ app/query/route.ts
@@ -1,5 +1,6 @@
 import { db } from "@vercel/postgres";
-
+import { sql } from '@vercel/postgres';
+import { Revenue, Customer } from '../lib/definitions';
 const client = await db.connect();
 
 async function listInvoices() {
@@ -13,13 +14,34 @@ async function listInvoices() {
 	return data.rows;
 }
 
+async function fetchRevenue() {
+  try {
+    // Artificially delay a response for demo purposes.
+    // Don't do this in production :)
+
+    // console.log('Fetching revenue data...');
+    // await new Promise((resolve) => setTimeout(resolve, 3000));
+
+    const data = await sql<Customer>`SELECT * FROM customers`;
+
+    // console.log('Data fetch completed after 3 seconds.');
+
+    return data.rows;
+  } catch (error) {
+    console.error('Database Error:', error);
+    throw new Error('Failed to fetch revenue data.');
+  }
+}
+
+
 export async function GET() {
   // return Response.json({
   //   message:
   //     'Uncomment this file and remove this line. You can delete this file when you are finished.',
   // });
   try {
-  	return Response.json(await listInvoices());
+  	// return Response.json(await listInvoices());
+    return Response.json(await fetchRevenue());
   } catch (error) {
   	return Response.json({ error }, { status: 500 });
   }
